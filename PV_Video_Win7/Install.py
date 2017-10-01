58a1cbac1e888a611cae06d0bf56dce4a787e1db
a6225301 <412827216@qq.com>
2017-10-01 15:45:19 +0800

    add xp/win7 folder

---
+++ PV_Video_Win7/Install.py
@@ -0,0 +1,10 @@
+import shutil
+import os
+
+shutil.copy('chromedriver.exe','C:\Python27\chromedriver.exe')
+
+os.system('pip install selenium-2.47.1-py2-none-any.whl')
+
+os.system('52.0.2743.116_chrome_installer.exe')
+
+
